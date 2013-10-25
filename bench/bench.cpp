05b6bb53b1ee8bd363e751bf6b98164f3000bd79
= <amidvidy@gmail.com>
2013-10-24 23:19:54 -0700

    record time elapsed during benchmark

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -5,31 +5,36 @@
 
 void hammerDB(CoarseGrainedDB *db, int nthreads, int nkeys) {
   std::thread threads[nthreads];
+
+  
   
   std::cout << "Starting Benchmark!" << std::endl;
+  auto start_time = std::chrono::high_resolution_clock::now();
 
   for (int i = 0; i < nthreads; ++i) {
     threads[i] = std::thread([=]() {
+	std::string thread_name = "foo";
 	for (int times = 0; times < nkeys; ++times) {
-	  db->put("foo", "bar");
+	  db->put("foo", thread_name);
 	}
     });
   }
   
   for (int i = 0; i < nthreads; ++i) {
     threads[i].join();
-    std::cout << "joined a thread!" << std::endl;
   }
+  auto end_time = std::chrono::high_resolution_clock::now();
+
   
   std::cout << "Finishing Benchmark!" << std::endl;
+  std::cout << "Total Duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count() << " us" << std::endl;
+  
 }
 
 int main(void) {
   CoarseGrainedDB *db = new CoarseGrainedDB;
-  db->put(std::string("adam"), std::string("midvidy"));
-  hammerDB(db, 400, 10000);
-  std::cout << "fooooooo!!!" << std::endl;
-  std::cout << db->get(std::string("foo")) << std::endl;
+  hammerDB(db, 100, 100000);
+  std::cout << db->get(std::string("foo")) <<  std::endl;
   return 0;
 }
 
