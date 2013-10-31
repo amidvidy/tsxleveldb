282a56ed44a6cbf08552712eba7222a1f1c98498
= <amidvidy@gmail.com>
2013-10-31 15:05:45 -0700

    add concurrent_hash_map to gine grained benchmark

--- bench/db.hpp
+++ bench/db.hpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <string>
 #include <mutex>
+#include <tbb/tbb.h>
+#include <tbb/concurrent_hash_map.h>
 
 class DB {
 public:
@@ -16,6 +18,7 @@ protected:
   virtual lock_ref lockr(std::string low, std::string high) = 0;
   */
 };
+
 
 
 class CoarseGrainedDB : public DB {
@@ -37,17 +40,31 @@ private:
   std::map<std::string, std::string> storage;
 };
 
+//
+// From: http://software.intel.com/sites/products/documentation/doclib/tbb_sa/help/tbb_userguide/concurrent_hash_map.htm
+class StringHash {
+  static std::size_t hash(const std::string& x) {
+    std::size_t h = 0;
+    for (const char* s = x.c_str(); *s; ++s) {
+      h = (h * 17) ^ * s;
+    }
+    return h;
+  }
+  //! True if strings are equal
+  static bool equal(const std::string& x, const std::string& y) {
+    return x == y;
+  }
+};
+
+typedef tbb::concurrent_hash_map<std::string, std::string, StringHash> TBBMap;
+
 class FineGrainedDB : public DB {
 public:
   FineGrainedDB() = default;
   std::string get(std::string key) override;
   void put(std::string key, std::string value) override;
-/*
 private:
-  void xlock(std::string key);
-  void slock(std::string key);
-  void unlock(std::string key);
-*/
+  TBBMap storage;
 };
 
 class LockElidedDB : public DB {
