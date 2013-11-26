a3308b416b87087311b135cf5d6806c2bc7583b5
Jeff Tsui <tsui.jeff@berkeley.edu>
2013-11-26 15:22:29 -0800

    update run script

--- runYCSB.sh
+++ runYCSB.sh
@@ -3,13 +3,13 @@ BASE_DAEMON=/home/jtsui/leveldb-daemon
 
 INPUT_WORKLOAD=tsxa
 
-cp $(BASE_LEVELDB)/libleveldb.a /usr/local/lib/
-cp -r $(BASE_LEVELDB)/include/leveldb /usr/local/include/
-cd $(BASE_DAEMON)/simpleleveldb
+cp $BASE_LEVELDB/libleveldb.a /usr/local/lib/
+cp -r $BASE_LEVELDB/include/leveldb /usr/local/include/
+cd $BASE_DAEMON/simpleleveldb
 make && sudo make install
-cd $(BASE_DAEMON)/simpleleveldb
+cd $BASE_DAEMON/simpleleveldb
 rm -rf tmp
 ./simpleleveldb --db-file=tmp
 
-./ycsb load leveldb -P workloads/$(INPUT_WORKLOAD) -s -p measurementtype=timeseries -p timeseries.granularity=10000 > load.log
-./ycsb run leveldb -P workloads/$(INPUT_WORKLOAD) -s -p measurementtype=timeseries -p timeseries.granularity=10000 > run.log
+./ycsb load leveldb -P workloads/$INPUT_WORKLOAD -s -p measurementtype=timeseries -p timeseries.granularity=10000 > load.log
+./ycsb run leveldb -P workloads/$INPUT_WORKLOAD -s -p measurementtype=timeseries -p timeseries.granularity=10000 > run.log
